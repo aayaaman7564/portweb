3c338bcedc8db7c789c781f9ed33df8a70fb926a
Aman Kumar <amankumar002003@gmail.com>
2023-09-04 03:16:09 +0530

    homepage change in layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Navigation from "../components/Navigation"
-import Hero from "../components/Hero";
-import Work from "../components/work";
-import Footer from "../components/footer";
+import Navigation from "./components/Navigation"
+import Hero from "./components/Hero";
+import Work from "./components/work";
+import Footer from "./components/footer";
 const Home = () =>{
     return(
         <div>
