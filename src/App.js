13dc7937a21d445f17460efd4b8c82203d53ac15
Aman Kumar <amankumar002003@gmail.com>
2023-09-04 03:02:19 +0530

    Change in homepage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
-import Navigation from "./components/Navigation"
-import Footer from "./components/footer";
-import Heroproject from "./components/Heroproject";
-import "./components/projectcardstyles.css";
-import Work from "./components/work";
-const Project = () =>{
-    return(
-        <div>
-            <Navigation />
-            <Heroproject />
-            <Work />
-            <Footer />
-        </div>
-    );
+import Home from "./Routes/Home";
+import Project from "./Routes/Project";
+import Contact from "./Routes/contact";
+import {Route, Routes} from "react-router-dom";
+
+function App(){
+  return(
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/Project" element={<Project />}/>
+        <Route path="/Contact" element={<Contact />}/>
+      </Routes>
+    </>
+  );
 }
-export default Project;
+export default App;
